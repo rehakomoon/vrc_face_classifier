45828cef8345a7f27f5708c0aaebcbd75a0eb6c1
rehakomoon <re.hako.moon@gmail.com>
2020-07-16 06:19:19 +0000

    bug fix

--- 07_convert_inferenced_data_to_annotation.py
+++ 07_convert_inferenced_data_to_annotation.py
@@ -14,7 +14,7 @@ import pickle
 
 data_path = Path.cwd() / "data"
 input_dir = data_path / "inference_data"
-output_dir = data_path / "inference_filtered_data"
+output_dir = data_path / "inference_data"
 pickle_path = input_dir / "inference.pkl"
 input_annotation_path = input_dir / "annotation.txt"
 
@@ -54,7 +54,6 @@ for (image_filename, image_size), areas in tqdm(inferred_data):
     filename = image_filename[:-4]
     output_txt_path = output_dir / (filename + ".txt")
     areas = areas[0]
-    #print(filename)
     
     areas = [(int(x0), int(y0), int(x1), int(y1)) for (x0, y0, x1, y1, p) in areas if p > confidence_threshold]
     
@@ -63,7 +62,7 @@ for (image_filename, image_size), areas in tqdm(inferred_data):
 
     annotation_text = [" ".join([str(d) for d in l]) + " 1" for l in areas]
     annotation_text = "\n".join(annotation_text)
-    annotation_text = f"{image.shape[1]} {image.shape[0]}\n{len(areas)}\n{annotation_text}"
+    annotation_text = f"{image_size}\n{len(areas)}\n{annotation_text}"
     
     with open(output_txt_path, "w") as f:
         f.write(annotation_text)
